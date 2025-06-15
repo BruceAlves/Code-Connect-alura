ce1deed436877c1630442165e16eb197a8262c0c
Bruce Alves <brucewallace144@gmail.com>
2025-06-15 12:59:28 -0300

    add use query

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Prompt } from "next/font/google";
 import { Aside } from "@/components/Aside";
 import "./globals.css";
 import { SearchForm } from "@/components/SearchForm";
+import { ReactQueryProvider } from "./ReactQueryProvider";
 
 export const metadata = {
   title: "Code Connect",
@@ -18,15 +19,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br" className={prompt.className}>
       <body>
-        <div className="app-container">
-          <div>
-            <Aside />
+        <ReactQueryProvider>
+          <div className="app-container">
+            <div>
+              <Aside />
+            </div>
+            <div className="main-content">
+              <SearchForm />
+              {children}
+            </div>
           </div>
-          <div className="main-content">
-            <SearchForm />
-            {children}
-          </div>
-        </div>
+        </ReactQueryProvider>
       </body>
     </html>
   );
