1483992afdefcd9d42e4a00e835dde34ee6b5456
Bruce Alves <brucewallace144@gmail.com>
2025-06-17 23:18:42 -0300

    the use prefetchQuery

--- src/app/hooks/UseFetchReplies.js
+++ src/app/hooks/UseFetchReplies.js
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query"
 
-const fetchReplies = async ({ commentId, slug }) => {
+export const fetchReplies = async ({ commentId, slug }) => {
     const response = await fetch(`/api/comment/${commentId}/replies?slug=${slug}`);
     if (!response.ok) {
         throw new Error("Erro ao carregar comentário");
@@ -9,12 +9,11 @@ const fetchReplies = async ({ commentId, slug }) => {
     return response.json();
 };
 
-
-
 export const useFetchReplies = ({ commentId, slug }) => {
     return useQuery({
-        queryKey: ["replies", commentId],
+        queryKey: ["replies", commentId, slug],
         queryFn: async () => fetchReplies({ commentId, slug }),
         enabled: !!commentId && !!slug,
+        retry: 5,
     });
 }
